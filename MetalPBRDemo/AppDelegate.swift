a08f985165361cc6cdc32746e034e5fd1f9f4587
Ivan <ivan.ushakov@gmail.com>
2019-04-18 13:51:32 +0300

    Initial Commit

---
+++ MetalPBRDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MetalPBRDemo
+//
+//  Created by  Ivan Ushakov on 18/04/2019.
+//  Copyright © 2019  Ivan Ushakov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
